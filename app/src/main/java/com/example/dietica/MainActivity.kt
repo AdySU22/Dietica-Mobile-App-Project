44c02f16dc4cb30a5b7c440213e3dc5fcca422a0
kumara <kumaraevanjanottama@gmail.com>
2024-09-22 23:34:11 +0700

    Opening and login page

--- app/src/main/java/com/example/dietica/MainActivity.kt
+++ app/src/main/java/com/example/dietica/MainActivity.kt
@@ -7,8 +7,6 @@ import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

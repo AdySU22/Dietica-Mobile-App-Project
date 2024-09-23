2c876411133b86a9647826551116f69ee71be254
kumara <kumaraevanjanottama@gmail.com>
2024-09-23 16:47:19 +0700

    Implementing FE in sign in page. Some colors might differ and the design haven't been implemented properly, because it will add blocker that slow down the development

--- app/src/main/java/com/example/dietica/SignInActivity.kt
+++ app/src/main/java/com/example/dietica/SignInActivity.kt
@@ -1,4 +1,12 @@
 package com.example.dietica
 
-class SignInActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignInActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
 }

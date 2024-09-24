f280da75bbfa4483948e008ff670560d6dcdf03d
kumara <kumaraevanjanottama@gmail.com>
2024-09-24 21:24:58 +0700

    Fixing import directives in OTPVerification.kt

--- app/src/main/java/com/example/dietica/OTPVerification.kt
+++ app/src/main/java/com/example/dietica/OTPVerification.kt
@@ -3,12 +3,11 @@ package com.example.dietica
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class OTPVerification : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_sign_in)
         }
     }

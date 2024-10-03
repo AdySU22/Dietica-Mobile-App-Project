1089d74d59f88d9f544edc4bb83512c5e93e18e7
kumara <kumaraevanjanottama@gmail.com>
2024-10-03 23:16:17 +0700

    Add ForgetPassword

--- app/src/main/java/com/example/dietica/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/dietica/ForgotPasswordActivity.kt
@@ -1,20 +1,20 @@
 package com.example.dietica
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.TextView
 
 class ForgotPasswordActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
+
     }
 }

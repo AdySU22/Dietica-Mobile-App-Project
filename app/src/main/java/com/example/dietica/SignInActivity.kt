05540707f3220bd817fca7c9db5dea43aaec7081
kumara <kumaraevanjanottama@gmail.com>
2024-10-03 11:30:53 +0700

    Refactoring Sign In and Sign Up page.

---
+++ app/src/main/java/com/example/dietica/SignInActivity.kt
@@ -0,0 +1,39 @@
+package com.example.dietica
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        val btnLogin: Button = findViewById(R.id.btnLogin)
+        val forgotPasswordText: TextView = findViewById(R.id.forgotPasswordText)
+        val signUpAccountText: TextView = findViewById(R.id.signUpAccountText)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+//        forgotPasswordText.setOnClickListener {
+//            forgotPassword()
+//        }
+
+        signUpAccountText.setOnClickListener {
+            signUpAccount()
+        }
+    }
+
+    fun signUpAccount() {
+        val intent = Intent(this, SignInActivity::class.java)
+        startActivity(intent)
+    }
+}
--- app/src/main/java/com/example/dietica/SignUpActivity.kt
+++ app/src/main/java/com/example/dietica/SignUpActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import android.widget.TextView
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -14,9 +15,22 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sign_up)
 
         val btnRegister: Button = findViewById(R.id.btnRegister)
+        val loginAccountText: TextView = findViewById(R.id.loginAccountText)
+
+
         btnRegister.setOnClickListener {
             val intent = Intent(this, OTPVerification::class.java)
             startActivity(intent)
         }
+
+        loginAccountText.setOnClickListener {
+            loginAccount()
+        }
     }
+
+    fun loginAccount() {
+val intent = Intent(this, SignInActivity::class.java)
+        startActivity(intent)
+    }
+
 }

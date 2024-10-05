f736d7f1fb8c51573feb336668b7413ae696b923
kumara <kumaraevanjanottama@gmail.com>
2024-10-05 19:47:09 +0700

    Add update password

--- app/src/main/java/com/example/dietica/UpdatePasswordActivity.kt
+++ app/src/main/java/com/example/dietica/UpdatePasswordActivity.kt
@@ -1,20 +1,23 @@
 package com.example.dietica
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
 
 class UpdatePasswordActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_update_password)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val updatePasswordButton: Button = findViewById(R.id.btnUpdatePassword)
+
+        updatePasswordButton.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
         }
     }
 }

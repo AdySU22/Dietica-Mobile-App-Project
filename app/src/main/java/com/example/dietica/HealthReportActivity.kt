d33d4d4cec71211f5143366f29a9d75ab46ef59e
kumara <kumaraevanjanottama@gmail.com>
2024-10-09 00:49:43 +0700

    Add layouts for Health Report

--- app/src/main/java/com/example/dietica/HealthReportActivity.kt
+++ app/src/main/java/com/example/dietica/HealthReportActivity.kt
@@ -1,20 +1,34 @@
 package com.example.dietica
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class HealthReportActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_health_report)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val backButton: ImageView = findViewById(R.id.backButton)
+        backButton.setOnClickListener {
+            finish()
         }
+
+        val graphButton: Button = findViewById(R.id.graphButton)
+        graphButton.setOnClickListener {
+            // Action to show more detailed graph
+            // Not yet implemented
+        }
+
+        val weeklyText: TextView = findViewById(R.id.weeklyText)
+        weeklyText.visibility = View.VISIBLE
     }
 }

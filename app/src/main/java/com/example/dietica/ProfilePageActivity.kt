a5c22913ddfc71bb955581e0d6186045bdafa427
kumara <kumaraevanjanottama@gmail.com>
2024-10-12 23:08:26 +0700

    Add redirect to sign in and edit profile

--- app/src/main/java/com/example/dietica/ProfilePageActivity.kt
+++ app/src/main/java/com/example/dietica/ProfilePageActivity.kt
@@ -1,20 +1,30 @@
 package com.example.dietica
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class ProfilePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_profile_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val logoutButton: Button = findViewById(R.id.logoutButton)
+        val editProfileButton: LinearLayout = findViewById(R.id.editProfileButton)
+
+        logoutButton.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
         }
+
+        editProfileButton.setOnClickListener {
+            val intent = Intent(this, EditProfile::class.java)
+            startActivity(intent)
+        }
+
     }
 }

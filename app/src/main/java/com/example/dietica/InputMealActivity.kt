228f88dc1716efe8d0c803c9a82cf72affcaa8a6
kumara <kumaraevanjanottama@gmail.com>
2024-10-13 23:10:00 +0700

    Fix error and add redirect

--- app/src/main/java/com/example/dietica/InputMealActivity.kt
+++ app/src/main/java/com/example/dietica/InputMealActivity.kt
@@ -1,6 +1,8 @@
 package com.example.dietica
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.*
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,6 +12,12 @@ class InputMealActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_input_meal)
 
+        val btnBack: Button = findViewById(R.id.btnBack)
+
+        btnBack.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }

c58c05a66f26020966cf312f67a21405707585cf
kumara <kumaraevanjanottama@gmail.com>
2024-10-15 21:06:44 +0700

    Add final nutrition layout

--- app/src/main/java/com/example/dietica/ManuallyInputMeal.kt
+++ app/src/main/java/com/example/dietica/ManuallyInputMeal.kt
@@ -1,6 +1,7 @@
 package com.example.dietica
 
 import android.os.Bundle
+import android.widget.*
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,48 @@ class ManuallyInputMeal : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_manually_input_meal)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnBack: Button = findViewById(R.id.btnBack)
+        val inputFoodName: EditText = findViewById(R.id.inputFoodName)
+        val inputServingType: EditText = findViewById(R.id.inputServingType)
+        val inputCalories: EditText = findViewById(R.id.inputCalories)
+        val inputFats: EditText = findViewById(R.id.inputFats)
+        val inputSaturatedFat: EditText = findViewById(R.id.inputSaturatedFat)
+        val inputUnsaturatedFat: EditText = findViewById(R.id.inputUnsaturatedFat)
+        val inputTransFat: EditText = findViewById(R.id.inputTransFat)
+        val inputCholesterol: EditText = findViewById(R.id.inputCholesterol)
+        val inputSodium: EditText = findViewById(R.id.inputSodium)
+        val inputCarbohydrates: EditText = findViewById(R.id.inputCarbohydrates)
+        val inputProtein: EditText = findViewById(R.id.inputProtein)
+        val inputSugar: EditText = findViewById(R.id.inputSugar)
+        val inputFiber: EditText = findViewById(R.id.inputFiber)
+        val btnCancel: Button = findViewById(R.id.btnCancel)
+        val btnSave: Button = findViewById(R.id.btnSave)
+
+        btnBack.setOnClickListener {
+            finish()
+        }
+
+        // Cancel Button Click Listener
+        btnCancel.setOnClickListener {
+            clearFields(
+                inputFoodName, inputServingType, inputCalories, inputFats,
+                inputSaturatedFat, inputUnsaturatedFat, inputTransFat,
+                inputCholesterol, inputSodium, inputCarbohydrates,
+                inputProtein, inputSugar, inputFiber
+            )
+        }
+
+        btnSave.setOnClickListener {
+            finish()
+        }
+
+    }
+
+    private fun clearFields(vararg fields: EditText) {
+        for (field in fields) {
+            field.text.clear()
         }
     }
+
 }

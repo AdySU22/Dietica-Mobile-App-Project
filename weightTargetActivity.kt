b24c1667159e80f86ae72a04d2f5d60faa003fbc
Vie Ain <115891461+VieViyViy@users.noreply.github.com>
2024-10-17 00:15:48 +0700

    Weight
    Add weight page and finish up ui look

---
+++ weightTargetActivity.kt
@@ -0,0 +1,28 @@
+package com.example.dietica
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class weightTargetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_weight_target)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val geBacktoExercise: ImageView = findViewById(R.id.leftArrow2)
+
+        geBacktoExercise.setOnClickListener {
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}

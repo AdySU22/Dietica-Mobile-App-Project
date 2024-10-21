2299a198ad78170c30c76cb67eeb064b4cf6940e
bryanrp <66724556+bryanrp@users.noreply.github.com>
2024-10-22 01:16:28 +0700

    Fix activity_exercise.xml and restructure files

--- app/src/main/java/com/example/dietica/WeightTargetActivity.kt
+++ app/src/main/java/com/example/dietica/WeightTargetActivity.kt
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class weightTargetActivity : AppCompatActivity() {
+class WeightTargetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()

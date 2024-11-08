3fcd5d0c49837f389b8f60017a7d34be9be6a3b0
kumara <kumaraevanjanottama@gmail.com>
2024-11-09 00:45:03 +0700

    Add changes to id

--- app/src/main/java/com/example/dietica/TodoActivity.kt
+++ app/src/main/java/com/example/dietica/TodoActivity.kt
@@ -15,26 +15,35 @@ class TodoActivity : AppCompatActivity() {
         setContentView(R.layout.activity_todo)
 
         val btnBack: ImageView = findViewById(R.id.btnBack)
-        val date1: TextView = findViewById(R.id.date1)
-        val date2: TextView = findViewById(R.id.date2)
-        val date3: TextView = findViewById(R.id.date3)
-        val dateFormat = SimpleDateFormat("dd\nMMM", Locale.getDefault())
+        val date1Day: TextView = findViewById(R.id.date1_day)
+        val date1Month: TextView = findViewById(R.id.date1_month)
+        val date2Day: TextView = findViewById(R.id.date2_day)
+        val date2Month: TextView = findViewById(R.id.date2_month)
+        val date3Day: TextView = findViewById(R.id.date3_day)
+        val date3Month: TextView = findViewById(R.id.date3_month)
+
+        val dayFormat = SimpleDateFormat("dd", Locale.getDefault())
+        val monthFormat = SimpleDateFormat("MMM", Locale.getDefault())
         val calendar = Calendar.getInstance()
 
+        // Set back button functionality
         btnBack.setOnClickListener {
             val intent = Intent(this, WeightTargetActivity::class.java)
             startActivity(intent)
         }
 
         // Set the middle date (current date)
-        date2.text = dateFormat.format(calendar.time)
+        date2Day.text = dayFormat.format(calendar.time)
+        date2Month.text = monthFormat.format(calendar.time)
 
         // Set previous day date
         calendar.add(Calendar.DAY_OF_YEAR, -1)
-        date1.text = dateFormat.format(calendar.time)
+        date1Day.text = dayFormat.format(calendar.time)
+        date1Month.text = monthFormat.format(calendar.time)
 
         // Set next day date
         calendar.add(Calendar.DAY_OF_YEAR, 2)
-        date3.text = dateFormat.format(calendar.time)
+        date3Day.text = dayFormat.format(calendar.time)
+        date3Month.text = monthFormat.format(calendar.time)
     }
 }

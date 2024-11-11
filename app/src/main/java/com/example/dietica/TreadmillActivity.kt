4570b50443c9978aaedecd930cabdfe713698a3d
kumara <kumaraevanjanottama@gmail.com>
2024-11-11 22:11:58 +0700

    Add treadmill page

---
+++ app/src/main/java/com/example/dietica/TreadmillActivity.kt
@@ -0,0 +1,28 @@
+package com.example.dietica
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TreadmillActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_treadmill)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val leftArrow2: ImageView = findViewById(R.id.leftArrow2)
+
+        leftArrow2.setOnClickListener {
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}

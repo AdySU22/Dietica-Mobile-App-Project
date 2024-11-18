0b62120f07fd135dd993947930601acb87c54b3b
kumara <kumaraevanjanottama@gmail.com>
2024-11-18 12:38:00 +0700

    Implement immersive full-screen mode

--- app/src/main/java/com/example/dietica/SignInActivity.kt
+++ app/src/main/java/com/example/dietica/SignInActivity.kt
@@ -3,6 +3,7 @@ package com.example.dietica
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
@@ -39,6 +40,14 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                )
 
         val auth = FirebaseAuth.getInstance()
         signInServices = SignInServices(this, auth)
--- app/src/main/java/com/example/dietica/SignUpActivity.kt
+++ app/src/main/java/com/example/dietica/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.example.dietica
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
@@ -22,6 +23,14 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sign_up)
+
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                )
 
         signUpServices = SignUpServices(this)
 

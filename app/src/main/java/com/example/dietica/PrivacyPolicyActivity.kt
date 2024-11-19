d5bc7fa844072472695bbacfc0c3bf0660d60db3
kumara <kumaraevanjanottama@gmail.com>
2024-11-19 10:04:07 +0700

    Add back button functionality in both page

--- app/src/main/java/com/example/dietica/PrivacyPolicyActivity.kt
+++ app/src/main/java/com/example/dietica/PrivacyPolicyActivity.kt
@@ -1,20 +1,22 @@
 package com.example.dietica
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.*
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class PrivacyPolicyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_privacy_policy)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val backBtn: Button = findViewById(R.id.backBtn)
+
+        backBtn.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/dietica/TermsAndConditionsActivity.kt
+++ app/src/main/java/com/example/dietica/TermsAndConditionsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.dietica
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,13 @@ class TermsAndConditionsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_terms_and_conditions)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val backBtn: Button = findViewById(R.id.backBtn)
+
+        backBtn.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
         }
+
     }
 }

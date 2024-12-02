824b638b7b3951708f3e9422f92b9a21c1a1caf3
kumara <kumaraevanjanottama@gmail.com>
2024-12-03 06:59:27 +0700

    Finish exercise page for user input

--- app/src/main/java/com/example/dietica/ExerciseActivity.kt
+++ app/src/main/java/com/example/dietica/ExerciseActivity.kt
@@ -1,20 +1,35 @@
 package com.example.dietica
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_exercise)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val leftArrow: ImageView = findViewById(R.id.leftArrow)
+        val exerciseNameEditText: EditText = findViewById(R.id.exerciseNameEditText)
+        val hoursEditText: EditText = findViewById(R.id.hoursEditText)
+        val minutesEditText: EditText = findViewById(R.id.minutesEditText)
+        val secondsEditText: EditText = findViewById(R.id.secondsEditText)
+        val btnCancel: Button = findViewById(R.id.btnCancel)
+        val btnSave: Button = findViewById(R.id.btnSave)
+
+        leftArrow.setOnClickListener {
+            val intent = Intent(this,MyExerciseActivity::class.java)
+            startActivity(intent)
         }
+
+        btnCancel.setOnClickListener {
+            val intent = Intent(this,MyExerciseActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }

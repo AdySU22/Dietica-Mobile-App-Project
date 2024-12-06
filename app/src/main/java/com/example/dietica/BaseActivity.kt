f19463a47ad8f1ac0f2b2bacc2fdbeceadcbb758
kumara <kumaraevanjanottama@gmail.com>
2024-12-06 17:37:50 +0700

    Handle navigation bar when keyboard is used

--- app/src/main/java/com/example/dietica/BaseActivity.kt
+++ app/src/main/java/com/example/dietica/BaseActivity.kt
@@ -1,14 +1,16 @@
 package com.example.dietica
 
+import android.graphics.Rect
 import android.os.Bundle
-import android.view.View
+import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 
 open class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-                applyImmersiveMode()
+        applyImmersiveMode()
+        handleKeyboardVisibility()
     }
 
     override fun onResume() {
@@ -25,4 +27,24 @@ open class BaseActivity : AppCompatActivity() {
                         or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 )
     }
-}
+
+    private fun handleKeyboardVisibility() {
+        val rootView = window.decorView
+        rootView.viewTreeObserver.addOnGlobalLayoutListener {
+            val rect = Rect()
+            rootView.getWindowVisibleDisplayFrame(rect)
+            val screenHeight = rootView.rootView.height
+            val keypadHeight = screenHeight - rect.bottom
+
+            if (keypadHeight > screenHeight * 0.15) {
+                window.decorView.systemUiVisibility = (
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        )
+            } else {
+                applyImmersiveMode()
+            }
+            }
+        }
+    }
